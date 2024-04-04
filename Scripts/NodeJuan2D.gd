545f41ce09c977d429397c84986ee070766e1969
Wilbert Arcila <warcilac9@gmail.com>
2024-04-03 20:10:37 -0600

    dsfasdlkfnasdklf

--- Scripts/NodeJuan2D.gd
+++ Scripts/NodeJuan2D.gd
@@ -9,9 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var current_tile: Vector2i = tile_map.local_to_map(global_position)
-	print(current_tile)
+	var current_tile: Vector2i = tile_map.map_to_local(global_position)
+	#print(current_tile)
 	
+func _unhandled_input(event):
+	var target_tile: Vector2i = tile_map.map(event.position)
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				print(target_tile)
+				
 
-	
-	
--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -1,16 +1,23 @@
-extends TileMap
+extends Node2D
 
-var GridSize = 4
-var Dic= {}
+@onready var tile_map =$"../TileMap"
 
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	for x in GridSize:
-		for y in GridSize:
-			Dic[str(Vector2(x,y))]={
-				"Type": "Grass",
-				"position":str(Vector2(x,y))
-			}
-			set_cell(0,Vector2(x,y), 0, Vector2i(0,0), 0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var juan = $NodeJuan2D
+	var current_tile: Vector2i = juan.local_to_map(global_position)
+	print(current_tile)
 	
+func _unhandled_input(event):
+	var target_tile: Vector2i = tile_map.local_to_map(event.position)
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				pass
+				#print(target_tile)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
